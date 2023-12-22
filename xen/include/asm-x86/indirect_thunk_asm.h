d5ea0282479382eb7bec90ca5b463bb7eecb44c9
Andrew Cooper <andrew.cooper3@citrix.com>
2023-12-22 18:01:37 +0000

    From 0b66d7ce3c0290eaad28bdafb35200052d012b14 Mon Sep 17 00:00:00 2001 x86: Drop INDIRECT_JMP
    Indirect JMPs which are not tailcalls can lead to an unwelcome form of
    speculative type confusion, and we've removed the uses of INDIRECT_JMP to
    compensate.  Remove the temptation to reintroduce new instances.
    
    This is part of XSA-456 / CVE-2024-2201.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/include/asm-x86/indirect_thunk_asm.h
+++ xen/include/asm-x86/indirect_thunk_asm.h
@@ -13,10 +13,9 @@ asm ( "\t.include \"asm/indirect_thunk_asm.h\"" );
 # endif
 #else
 
-.macro INDIRECT_BRANCH insn:req arg:req
+.macro INDIRECT_CALL arg:req
 /*
- * Create an indirect branch.  insn is one of call/jmp, arg is a single
- * register.
+ * Create an indirect call.  arg is a single register.
  *
  * With no compiler support, this degrades into a plain indirect call/jmp.
  * With compiler support, dispatch to the correct __x86_indirect_thunk_*
@@ -26,7 +25,7 @@ asm ( "\t.include \"asm/indirect_thunk_asm.h\"" );
         $done = 0
         .irp reg, ax, cx, dx, bx, bp, si, di, 8, 9, 10, 11, 12, 13, 14, 15
         .ifeqs "\arg", "%r\reg"
-            \insn __x86_indirect_thunk_r\reg
+            call __x86_indirect_thunk_r\reg
             $done = 1
            .exitm
         .endif
@@ -39,15 +38,6 @@ asm ( "\t.include \"asm/indirect_thunk_asm.h\"" );
     .else
         \insn *\arg
     .endif
-.endm
-
-/* Convenience wrappers. */
-.macro INDIRECT_CALL arg:req
-    INDIRECT_BRANCH call \arg
-.endm
-
-.macro INDIRECT_JMP arg:req
-    INDIRECT_BRANCH jmp \arg
 .endm
 
 #endif
