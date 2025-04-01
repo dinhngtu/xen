0a6843c1902a151e5195e83bda9c86238132cb20
Andrew Cooper <andrew.cooper3@citrix.com>
2025-04-01 14:59:01 +0100

    From 3aa74473ccbf2a8f5f19b1a8b83d29a2c944f8dc Mon Sep 17 00:00:00 2001 [PATCH 07/21] x86/idle: Move monitor()/mwait() wrappers into
     cpu-idle.c
    Content-Type: text/plain; charset = "utf-8"
    Content-Transfert-Encoding: 8bit
    
    They're not used by any other translation unit, so shouldn't live in
    asm/processor.h, which is included almost everywhere.
    
    Our new toolchain baseline knows the MONITOR/MWAIT instructions, so use them
    directly rather than using raw hex.
    
    Change the hint/extention parameters from long to int.  They're specified to
    remain 32bit operands even 64-bit mode.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    (cherry picked from commit 61e10fc28ccddff7c72c14acec56dc7ef2b155d1)
    
    Backported-by: Thierry Escande <thierry.escande@vates.tech>
    Backport notes:
    - Minor index changes

--- xen/arch/x86/acpi/cpu_idle.c
+++ xen/arch/x86/acpi/cpu_idle.c
@@ -58,6 +58,19 @@
 
 /*#define DEBUG_PM_CX*/
 
+static always_inline void monitor(
+    const void *addr, unsigned int ecx, unsigned int edx)
+{
+    asm volatile ( "monitor"
+                   :: "a" (addr), "c" (ecx), "d" (edx) );
+}
+
+static always_inline void mwait(unsigned int eax, unsigned int ecx)
+{
+    asm volatile ( "mwait"
+                   :: "a" (eax), "c" (ecx) );
+}
+
 #define GET_HW_RES_IN_NS(msr, val) \
     do { rdmsrl(msr, val); val = tsc_ticks2ns(val); } while( 0 )
 #define GET_MC6_RES(val)  GET_HW_RES_IN_NS(0x664, val)
@@ -461,7 +474,7 @@ void mwait_idle_with_hints(unsigned int eax, unsigned int ecx)
         mb();
     }
 
-    __monitor(monitor_addr, 0, 0);
+    monitor(monitor_addr, 0, 0);
     smp_mb();
 
     /*
@@ -475,7 +488,7 @@ void mwait_idle_with_hints(unsigned int eax, unsigned int ecx)
         cpumask_set_cpu(cpu, &cpuidle_mwait_flags);
 
         spec_ctrl_enter_idle(info);
-        __mwait(eax, ecx);
+        mwait(eax, ecx);
         spec_ctrl_exit_idle(info);
 
         cpumask_clear_cpu(cpu, &cpuidle_mwait_flags);
@@ -905,9 +918,9 @@ void acpi_dead_idle(void)
              */
             mb();
             clflush(mwait_ptr);
-            __monitor(mwait_ptr, 0, 0);
+            monitor(mwait_ptr, 0, 0);
             mb();
-            __mwait(cx->address, 0);
+            mwait(cx->address, 0);
         }
     }
     else if ( (current_cpu_data.x86_vendor &
