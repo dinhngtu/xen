c81d952b6e76943f0d3536d68dfc6e17ce4e840a
Andrew Cooper <andrew.cooper3@citrix.com>
2025-07-01 21:04:17 +0100

    From d65c3fc9fd84ef952501a2f02270a136796f4822 Mon Sep 17 00:00:00 2001 [PATCH 14/21] xen/softirq: Rework arch_skip_send_event_check() into
     arch_set_softirq()
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit
    Content-Type: text/plain; charset = "utf-8"
    Content-Transfert-Encoding: 8bit
    
    x86 is the only architecture wanting an optimisation here, but the
    test_and_set_bit() is a store into the monitored line (i.e. will wake up the
    target) and, prior to the removal of the broken IPI-elision algorithm, was
    racy, causing unnecessary IPIs to be sent.
    
    To do this in a race-free way, the store to the monited line needs to also
    sample the status of the target in one atomic action.  Implement a new arch
    helper with different semantics; to make the softirq pending and decide about
    IPIs together.  For now, implement the default helper.  It will be overridden
    by x86 in a subsequent change.
    
    No functional change.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    (cherry picked from commit b473e5e212e445d3c193c1c83b52b129af571b19)
    
    Backported-by: Thierry Escande <thierry.escande@vates.tech>
    Backport notes:
    - Minor index changes
    - File xen/include/asm-x86/softirq.h not yet moved to
      xen/arch/x86/include/asm/softirq.h

--- xen/arch/x86/acpi/cpu_idle.c
+++ xen/arch/x86/acpi/cpu_idle.c
@@ -431,11 +431,6 @@ static int __init cpu_idle_key_init(void)
 }
 __initcall(cpu_idle_key_init);
 
-bool arch_skip_send_event_check(unsigned int cpu)
-{
-    return false;
-}
-
 void mwait_idle_with_hints(unsigned int eax, unsigned int ecx)
 {
     unsigned int cpu = smp_processor_id();
